7783a0bfa8c9274d48480df763b1159de3c8a7ee
zillaci <sun.yang@samsung.com>
2015-06-25 12:42:15 +0900

    Left drawer commit

--- app/src/main/java/com/pivotal_er/ciad/callitaday/MainActivity.java
+++ app/src/main/java/com/pivotal_er/ciad/callitaday/MainActivity.java
@@ -1,17 +1,30 @@
 package com.pivotal_er.ciad.callitaday;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] mMenuTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMenuTitles = getResources().getStringArray(R.array.array_menu_titles);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, mMenuTitles));
     }
 
     @Override
