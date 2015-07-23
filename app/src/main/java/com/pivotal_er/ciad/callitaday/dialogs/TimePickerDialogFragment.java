88bf22deab27a01a4014928f72725ee71c2010b6
zillaci <zillaci@nate.com>
2015-07-23 16:59:26 +0900

    fdgf

---
+++ app/src/main/java/com/pivotal_er/ciad/callitaday/dialogs/TimePickerDialogFragment.java
@@ -0,0 +1,16 @@
+package com.pivotal_er.ciad.callitaday.dialogs;
+
+import android.app.TimePickerDialog;
+import android.support.v4.app.DialogFragment;
+import android.widget.TimePicker;
+
+/**
+ * Created by student on 2015-07-23.
+ */
+public class TimePickerDialogFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+    }
+}
